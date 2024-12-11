97092ece06b4726fe5c9b01ac6f66585f25a8557
Wilson Neto <wilson.neto@redesaoroque.com.br>
2024-12-11 11:50:49 -0300

    feat: created Places component

---
+++ mobile/src/components/places/index.tsx
@@ -0,0 +1,41 @@
+import { Text, useWindowDimensions } from "react-native"
+import { useRef } from "react";
+import BottomSheet, { BottomSheetFlashList } from "@gorhom/bottom-sheet";
+
+import { Place, PlaceProps } from "@/components/place";
+import { s } from "./style";
+
+type Props = {
+  data: PlaceProps[]
+}
+
+export function Places({ data }: Props) {
+  const dimensions = useWindowDimensions()
+  const bottomSheetRef = useRef<BottomSheet>(null)
+
+  const snapPoints = {
+    min: 278,
+    max: dimensions.height - 128
+  }
+
+  return (
+    <BottomSheet
+      ref={bottomSheetRef}
+      snapPoints={[snapPoints.min, snapPoints.max]}
+      handleIndicatorStyle={s.indicator}
+      style={s.container}
+      enableOverDrag={false}
+    >
+      <BottomSheetFlashList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Place data={item} />}
+        contentContainerStyle={s.content}
+        ListHeaderComponent={() => (
+          <Text style={s.title}>Explore locais perto de você</Text>
+        )}
+        showsVerticalScrollIndicator={false}
+      />
+    </BottomSheet>
+  )
+}
---
+++ mobile/src/components/places/style.ts
@@ -0,0 +1,25 @@
+import { colors } from "@/styles/colors";
+import { fontFamily } from "@/styles/font-family";
+import { StyleSheet } from "react-native";
+
+export const s = StyleSheet.create({
+  container: {
+    backgroundColor: colors.gray[100],
+  },
+  content: {
+    gap: 12,
+    padding: 24,
+    paddingBottom: 100,
+  },
+  indicator: {
+    width: 80,
+    height: 4,
+    backgroundColor: colors.gray[300],
+  },
+  title: {
+    color: colors.gray[600],
+    fontSize: 16,
+    fontFamily: fontFamily.regular,
+    marginBottom: 16,
+  },
+});
