af435b22573a75f6fbb79c8b176ccf40da1fe321
Wilson Neto <wilson.neto@redesaoroque.com.br>
2024-12-12 09:46:30 -0300

    feat: created Details component from Market

---
+++ mobile/src/components/market/details/index.tsx
@@ -0,0 +1,48 @@
+import { Text, View } from "react-native"
+import { IconMapPin, IconPhone, IconTicket } from "@tabler/icons-react-native"
+
+import { s } from "./style"
+import { Info } from "@/components/market/info";
+
+export type PropsDetails = {
+  name: string
+  description: string
+  address: string
+  phone: string
+  coupons: number
+  rules: {
+    id: string
+    description: string
+  }[]
+}
+
+type Props = {
+  data: PropsDetails
+}
+
+export function Details({ data }: Props) {
+  return(
+    <View style={s.container}>
+      <Text style={s.name}>{data.name}</Text>
+      <Text style={s.description}>{data.description}</Text>
+
+      <View style={s.group}>
+        <Text style={s.title}>Informações</Text>
+
+        <Info icon={IconTicket} description={`${data.coupons} cupons disponíveis`} />
+        <Info icon={IconMapPin} description={data.address} />
+        <Info icon={IconPhone} description={data.phone} />
+      </View>
+
+      <View style={s.group}>
+        <Text style={s.title}>Regulamento</Text>
+
+        {data.rules.map((item) => (
+          <Text key={item.id} style={s.rule}>
+            {`\u2022 ${item.description}`}
+          </Text>
+        ))}
+      </View>
+    </View>
+  )
+}
